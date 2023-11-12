cc8fc9cfe5d0e796af11f91f43513b3ea86bb2e1
Gritflag <kjh8140532@naver.com>
2023-11-12 15:18:00 +0900

    update 3

--- tcp-server.py
+++ tcp-server.py
@@ -15,7 +15,7 @@ while 1:
     print('Client address:', addr)
     data= conn.recv (BUFFER_SIZE)
     #if not data: break
-    currentTime = " "+" new server3 !!! " + time.ctime (time.time()) + "\r\n"
+    currentTime = " "+" new server3_1 !!! " + time.ctime (time.time()) + "\r\n"
     print (data.decode('utf-8'))
     data = data + currentTime.encode('ascii')
     conn.send(data) # echo
